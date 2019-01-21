f1ec4ef5057679cc7602b92fbe1ac8242449a20f
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-21 23:19:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function destructivelyAppendKitten(name){
      return kittens;
 }
 
-function destructivelyPrependKitten(){
-  
+function destructivelyPrependKitten(name){
+     kittens.unshift(name);
+     return kittens;
 }
